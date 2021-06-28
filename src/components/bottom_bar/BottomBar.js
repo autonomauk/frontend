604aa484b155a7c0cedefdc87a8a6c24b430e931
iwishiwasaneagle <jh.ewers@gmail.com>
2021-06-28 12:34:10 +0100

    Tipping

--- src/components/bottom_bar/BottomBar.js
+++ src/components/bottom_bar/BottomBar.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import {Row} from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 
 import moment from 'moment';
 
 import './BottomBar.scss';
+import Tipping from '../tipping/Tipping';
 
-export default class BottomBar extends React.Component{
-    render(){
-        return <div className='footer'>
-            <Row className='justify-content-center'>
-                <a href='/TOS.html'><p>Terms of Service</p></a>
-                &#160;&#8226;&#160;
-                <a href='https://janhendrikewers.uk'><p> &#169; {moment().format('YYYY')} Jan-Hendrik Ewers</p></a>
-                &#160;&#8226;&#160;
-                <a href='/privacy.html'><p>Privacy Policy</p></a>
+export default class BottomBar extends React.Component {
+    render() {
+        return <footer>
+            <Tipping />
+            <Row id='info-row' sm={12}>
+                <div className='info-col'><a href='/TOS.html'><p>Terms of Service</p></a></div>
+
+                {/* <Col className='info-col'>&#160;&#8226;&#160;</Col> */}
+
+                <div className='info-col'><a href='https://janhendrikewers.uk'><p> &#169; {moment().format('YYYY')} Jan-Hendrik Ewers</p></a></div>
+
+                {/* <Col className='info-col'>&#160;&#8226;&#160;</Col> */}
+
+                <div className='info-col'><a href='/privacy.html'><p>Privacy Policy</p></a></div>
             </Row>
-        </div>
+        </footer>
     }
 }
---
+++ src/components/tipping/Tipping.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import bitcoin from '../../img/tipping/bitcoin.svg';
+import ethereum from '../../img/tipping/ethereum.svg';
+import dogecoin from '../../img/tipping/dogecoin.svg';
+import { Row, OverlayTrigger, Tooltip } from 'react-bootstrap';
+
+import './Tipping.scss';
+
+export default class Tipping extends React.Component{
+    render(){
+        return <OverlayTrigger
+        placement='top'
+        key='top'
+        overlay={
+            <Tooltip id='plz-donate'>
+                Asking for donations isn't something I want to be doing, but sadly the reality is that I need to pay for the server and domain. This webapp is more or less a one-time use to set up the functionality and thus there is no ad revenue to support it.
+            </Tooltip>
+        }
+        >
+        <Row className='tipping-row'>
+            <Row className='tipping-col'><img src={bitcoin} alt="BTC logo" height='24'></img>&#160;{process.env.REACT_APP_BITCOIN_ADDRESS}</Row>
+            <Row className='tipping-col'><img src={ethereum} alt="ETH logo" height='24'></img>&#160;{process.env.REACT_APP_ETHEREUM_ADDRESS}</Row>
+            <Row className='tipping-col'><img src={dogecoin} alt="DOGE logo" height='24'></img>&#160;{process.env.REACT_APP_DOGECOIN_ADDRESS}</Row>
+        </Row>
+        </OverlayTrigger>
+    }
+}
