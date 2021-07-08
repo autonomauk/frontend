c1c6f2171e7a3a6197d0ec65fb6d63381e9b4b86
Jan-Hendrik Ewers <jh.ewers@gmail.com>
2021-07-09 00:01:52 +0100

    Rename (#22)
    * Rename
    
    * Name change artefacts

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends React.Component {
       <div id='App'>
         <div className="content">
           <Col xs={12} sm={8} md={8} lg={5} id='app-col'>
-            <h1 id="title" className='display-3'>Spotify Playlister</h1>
+            <h1 id="title" className='display-3'>AUTONOMA</h1>
             <p>This app adds any new <b>favourited/saved</b> track to a monthly playlist once you "liked" it on Spotify. This generally takes 1-2 minutes to update. Other than logging in, there is nothing else for you to do! Sit back and relax.</p>
             {this.state.jwt ? <User /> : <Login />}
           </Col>
