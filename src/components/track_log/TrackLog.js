998719ce0289aa838141d3282fd79b9f1530b85e
Jan-Hendrik Ewers <jh.ewers@gmail.com>
2021-07-16 15:04:24 +0100

    Track log implementation on frontend (#26)
    * New routing structure. More pydantic models. New route 'track_log' for seeing what's been added
    
    * Testing
    
    * Testing
    
    * Prevent this from auto-starting
    
    * Log tracks being added + testing
    
    * Preloading css caused the fonts to not work
    
    * https://drafts.csswg.org/css-fonts/#font-fetching-requirements
    
    * Wrong model
    
    * New history log
    
    * Be gone

--- src/components/track_log/TrackLog.js
+++ src/components/track_log/TrackLog.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import {Col, Spinner, Row} from 'react-bootstrap';
+import { Col, Spinner, Row } from 'react-bootstrap';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import './TrackLog.scss';
+import moment from 'moment';
 
 export default function TrackLog(props) {
-    const [track_log, setTrackLog] = React.useState([])
+    const [track_logs, setTrackLogs] = React.useState([])
     const [loading, setLoading] = React.useState(true)
     const { jwt } = props
 
@@ -14,28 +17,59 @@ export default function TrackLog(props) {
                 jwt: jwt
             }
         }).then(res => res.json())
-            .then(setTrackLog)
+            .then(setTrackLogs)
             .then(_ => setLoading(false))
             .catch(err => console.error(err));
     }, []);
 
-    return <Col>{loading ? <Spinner animation="border" /> : track_log.map((track,ind)=><Track key={"track_"+ind} track={track}/>)}</Col>
+    return <Col id='track-log'>{
+        loading ?
+            <Spinner animation="border" />
+            :
+                <div>
+                    <h4>History</h4>
+                    <div id='break' />
+                                <div id='track-list'>
+                    {track_logs.map((track_log, idx) => <Track key={'track_' + idx} track_log={track_log} />)}
+                                </div>
+                </div>
+    }
+    </Col>
 }
 
-function Track(props){
-    const {createdAt, name, uri, artists, album} = props.track
-    const track_url = "https://open.spotify.com/track/" + uri.split(":")[2]
+function Track(props) {
+    const { createdAt, track, playlist } = props.track_log
 
-    return <a href={track_url}>
-        <Row className='track'>
-                <img src={album.image_url} className='track-img'/>
-            <Col>
-                <h6>{name}</h6>
-                <p>{album.name}</p>
-                <p>{artists[0].name}</p>
-                <p>Added at {createdAt}</p>
-            </Col>
-        </Row>
-    </a>
+    const track_url = "https://open.spotify.com/track/" + track.uri.split(":").pop();
+    const album_url = "https://open.spotify.com/album/" + track.album.uri.split(':').pop();
+    const artist_url = "https://open.spotify.com/artist/" + track.artists[0].uri.split(':').pop();
+    const playlist_url = "https://open.spotify.com/playlist/" + playlist.uri.split(':').pop();
 
+    const artists_str = track.artists.length == 1 ? 
+        track.artists[0].name 
+        : 
+        track.artists.map((artist, _) => artist.name).join(', ');
+
+    const addedAt_str = moment(createdAt, 'YYYY-MM-DDThh:mm:ss.s').fromNow()
+    console.log(addedAt_str,createdAt)
+    return <Row className='track-row'>
+        <Col className="track-img-col"><a href={album_url} className='track-img'><img src={track.album.image_url} className='track-img'  /></a></Col>
+        <Col className='track-text-col'>
+            <div className="track-text">
+                <a href={track_url}>
+                    <strong>{track.name}</strong>
+                </a>
+            </div>
+            <div className="track-text">
+                <a href={artist_url}>
+                    {artists_str}
+                </a>
+            </div>
+            <div className="track-text">
+                <a href={playlist_url}>
+                    Added to <em>{playlist.name}</em> {addedAt_str}
+                </a>
+            </div>
+        </Col>
+    </Row>
 }
